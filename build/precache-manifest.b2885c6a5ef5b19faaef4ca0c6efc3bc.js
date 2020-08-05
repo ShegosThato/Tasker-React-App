58b695d3d072125749128b61e9aae5e890c64ae3
Thato Mashego <cjmashego66@gmail.com>
2020-08-06 00:35:40 +0200

    First deploy: ran "npm run build"

---
+++ build/precache-manifest.b2885c6a5ef5b19faaef4ca0c6efc3bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc334d1c280dd4f64527bb68c6c70ded",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48f489dfaf26fad2135a",
+    "url": "/static/css/main.88b65862.chunk.css"
+  },
+  {
+    "revision": "e9dea2b3a6a67e3936a9",
+    "url": "/static/js/2.f527adea.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f527adea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f489dfaf26fad2135a",
+    "url": "/static/js/main.77f2b0b1.chunk.js"
+  },
+  {
+    "revision": "514277e4970c28a3b3f1",
+    "url": "/static/js/runtime-main.0e76ac4f.js"
+  }
+]);

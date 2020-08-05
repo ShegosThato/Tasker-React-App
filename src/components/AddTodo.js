33c721a92eac19f72833436d195f088439345d68
Thato Mashego <cjmashego66@gmail.com>
2020-08-06 00:28:22 +0200

    Added minor UI tweaks

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -25,7 +25,7 @@ class AddTodo extends Component {
         return (
             <form className="form pl-5 pr-2 mb-1" onSubmit={this.submitData}>
                 <div className="form-group row mx-auto" >
-                    <input type="text" className="form-control col-9" name="title" onChange={this.updateState} placeholder="Enter your title here..." value={this.state.title} />
+                    <input type="text" className="form-control col-9" name="title" onChange={this.updateState} placeholder="Enter your task here..." value={this.state.title} />
                     <button type="submit" className="btn btn-outline-primary ml-3" onClick={this.submitData}>Add</button>
                 </div>
             </form>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ export default function Header({ addTodo }) {
     return (
         <div className="jumbotron jumbotron-fluid text-center">
             <h1 className="display-2 mb-5">Tasker</h1>
+            <small className="text-muted">by Thato Mashego</small>
             <AddTodo addTodo={addTodo} />
         </div>
     )
